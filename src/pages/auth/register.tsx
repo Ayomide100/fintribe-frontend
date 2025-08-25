404fd0b928af14fb5b0cab3900538a9641eeb0d0
Dhanielgeek <dhanielknightz46@gmail.com>
2025-08-25 17:35:43 +0100

    new  Update on the app...

---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+import herosect from "../../../assets/fintribe 1.png";
+
+const Register = () => {
+  return (
+    <div
+      className="w-full h-[100vh] flex justify-center items-center"
+      style={{
+        background:
+          "linear-gradient(to left, #CFE7DB, #E8EFF7, #FFFFFF, #FEFFFE, #FCFEFD)",
+      }}
+    >
+      <div className="w-[60%] h-full"></div>
+      <div className="w-[40%] h-full flex justify-center items-center">
+        <Image
+          src={herosect}
+          alt="Description of image"
+          className="w-[92%] h-[92%] object-cover rounded-2xl"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Register;
