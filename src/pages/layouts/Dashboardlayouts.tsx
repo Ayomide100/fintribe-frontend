e88e352a3395fcd1f3fee30d2c15ed570668bf06
Dhanielgeek <dhanielknightz46@gmail.com>
2025-09-22 14:10:20 +0100

    test.

--- src/pages/layouts/Dashboardlayouts.tsx
+++ src/pages/layouts/Dashboardlayouts.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { ReactNode, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import DashboardHeader from "../dashboard/dashboardheader";
